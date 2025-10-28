ce85ab26ac35ed8fa4283994d8c6d97c63796a97
Joshua Barbosa <47535132+JBarbosa07@users.noreply.github.com>
2025-10-28 22:19:20 +0000

    modified HW4 with corrections and added HW5

---
+++ 05-deployment/homework/README.md
@@ -0,0 +1,13 @@
+Answers
+
+Q1: uv version is 0.9.5.
+
+Q2: Scikit-learn hash is sha256:b4fc2525eca2c69a59260f583c56a7557c6ccdf8deafdba6e060f94c1c59738e.
+
+Q3: Probability of conversion = 0.5336072702798061.
+
+Q4: Probability of conversion = 0.5340417283801275.
+
+Q5: Base image size is 121 MB.
+
+Q6:
---
+++ 05-deployment/homework/predict.py
@@ -0,0 +1,84 @@
+import pickle
+import uvicorn
+
+from fastapi import FastAPI
+from typing import Dict, Any, Literal, Optional
+from pydantic import BaseModel, Field, ConfigDict
+
+# Referred to ChatGPT to structure the class. Commented out features not relevant to the assignment.
+class Client(BaseModel):
+    lead_source: Literal[
+        "paid_ads",
+        "social_media",
+        "events",
+        "referral",
+        "organic_search",
+        "NA"
+    ]
+
+    # industry: Literal[
+    #     "retail",
+    #     "healthcare",
+    #     "education",
+    #     "manufacturing",
+    #     "technology",
+    #     "finance",
+    #     "other",
+    #     "NA"
+    # ]
+
+    # employment_status: Literal[
+    #     "unemployed",
+    #     "employed",
+    #     "self_employed",
+    #     "student",
+    #     "NA"
+    # ]
+
+    # location: Literal[
+    #     "south_america",
+    #     "australia",
+    #     "europe",
+    #     "africa",
+    #     "middle_east",
+    #     "north_america",
+    #     "asia",
+    #     "NA"
+    # ]
+
+    number_of_courses_viewed: int = Field(..., ge=0)
+    annual_income: float = Field(..., ge=0.0)
+    # interaction_count: int = Field(..., ge=0)
+    # lead_score: float = Field(..., ge=0.0, le=1.0)
+
+
+class PredictResponse(BaseModel):
+    convert_probability: float
+    converted: bool
+
+
+# Response
+app = FastAPI(title="client-conversion-prediction")
+
+
+with open('pipeline_v1.bin', 'rb') as f_in:
+    pipeline = pickle.load(f_in)
+
+
+def predict_single(client):
+    result = pipeline.predict_proba(client)[0, 1]
+    return float(result)
+
+
+@app.post("/predict")
+def predict(client: Client) -> PredictResponse:
+    prob = predict_single(client.model_dump())
+
+    return PredictResponse(
+        convert_probability=prob,
+        converted=prob >= 0.5
+    )
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=9696)
