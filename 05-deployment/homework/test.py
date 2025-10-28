ad670b2d0b46cac95c53db10eff63591de484c64
Joshua Barbosa <47535132+JBarbosa07@users.noreply.github.com>
2025-10-28 22:33:12 +0000

    corrected HW4 and finished HW5

--- 05-deployment/homework/test.py
+++ 05-deployment/homework/test.py
@@ -1,13 +1,12 @@
 import requests
 
-url = 'http://localhost:9696/predict'
+url = 'https://predict-convert.fly.dev/predict'
 
 client = {
     "lead_source": "organic_search",
     "number_of_courses_viewed": 4,
     "annual_income": 80304.0
 }
-
 
 response = requests.post(url, json=client).json()
 
